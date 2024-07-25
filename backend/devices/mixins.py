477068be62a67fced5637283c65c53dbd6c2e2de
Amartya Gunjan <53094015+agile-flight@users.noreply.github.com>
2024-07-24 22:19:12 -0500

    Reverted to old search mixin

--- backend/devices/mixins.py
+++ backend/devices/mixins.py
@@ -1,8 +1,6 @@
 import os
 from haystack.query import SearchQuerySet
-from rest_framework.response import Response
-from rest_framework import generics
-from .pagination import CustomPagination  # Ensure the correct import path
+
 
 class SearchAndLimitMixin:
     search_fields = []
@@ -13,8 +11,6 @@ class SearchAndLimitMixin:
     def search_queryset(self, queryset, search_query):
         if search_query:
             sqs = SearchQuerySet().models(self.queryset.model).filter(content=search_query)
-            for field in self.search_fields:
-                sqs = sqs.filter(**{f"{field}__icontains": search_query})
             object_ids = [result.pk for result in sqs]
             pk_field = self.queryset.model._meta.pk.name
             queryset = queryset.filter(**{f"{pk_field}__in": object_ids})
@@ -23,19 +19,12 @@ class SearchAndLimitMixin:
     def get_queryset(self):
         queryset = super().get_queryset()
         search_query = self.request.query_params.get('search', None)
+        limit = self.request.query_params.get('limit', None)
 
         queryset = self.search_queryset(queryset, search_query)
         
-        paginator = CustomPagination()
-        page = self.request.query_params.get('page', 1)
-        paginated_queryset = paginator.paginate_queryset(queryset, self.request)
+        # Apply limit after all other query modifications
+        if limit:
+            queryset = queryset[:int(limit)]
         
-        return paginated_queryset
-
-    def list(self, request, *args, **kwargs):
-        queryset = self.get_queryset()
-        page = self.request.query_params.get('page', 1)
-        paginator = CustomPagination()
-        page = paginator.paginate_queryset(queryset, request)
-        serializer = self.get_serializer(page, many=True)
-        return paginator.get_paginated_response(serializer.data)
+        return queryset
