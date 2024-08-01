c69f82194d9d879172495dd65c47817e75e3c7bf
Superamaja <38116622+Superamaja@users.noreply.github.com>
2024-07-31 23:04:12 -0400

    Remove refresh tokens from login

--- backend/settings.py
+++ backend/settings.py
@@ -107,22 +107,12 @@ HAYSTACK_SIGNAL_PROCESSOR = "haystack.signals.RealtimeSignalProcessor"
 
 # JWT settings
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
-    "ROTATE_REFRESH_TOKENS": True,
-    "BLACKLIST_AFTER_ROTATION": True,
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=14),
     "ALGORITHM": "HS256",
     "SIGNING_KEY": settings.SECRET_KEY,
-    "VERIFYING_KEY": None,
     "AUTH_HEADER_TYPES": ("Bearer",),
     "USER_ID_FIELD": "id",
     "USER_ID_CLAIM": "user_id",
-    "AUTH_TOKEN_CLASSES": ("rest_framework_simplejwt.tokens.AccessToken",),
-    "TOKEN_TYPE_CLAIM": "token_type",
-    "JTI_CLAIM": "jti",
-    "SLIDING_TOKEN_REFRESH_EXP_CLAIM": "refresh_exp",
-    "SLIDING_TOKEN_LIFETIME": timedelta(minutes=5),
-    "SLIDING_TOKEN_REFRESH_LIFETIME": timedelta(days=1),
 }
 
 AUTHENTICATION_BACKENDS = [
--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -76,7 +76,6 @@ class UserLoginSerializer(serializers.Serializer):
 
         refresh = RefreshToken.for_user(user)
         access_token = str(refresh.access_token)
-        refresh_token = str(refresh)
 
         expires_at = timezone.now() + settings.SIMPLE_JWT["ACCESS_TOKEN_LIFETIME"]
 
@@ -92,7 +91,6 @@ class UserLoginSerializer(serializers.Serializer):
             "user_id": user.id,
             "tokens": {
                 "access_token": access_token,
-                "refresh_token": refresh_token,
                 "expires_at": expires_at,
             },
         }
